ddfa1597591456a9e8dd185d367d3dcc870856f3
Ego-Tyto <rithy.cappellozza.14-2@accademiadigitaleliguria.it>
2025-06-19 11:42:14 +0200

    Create Program.cs

---
+++ ConsoleApp1/ClassLibrary1/Calcolatrice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Calcolatrice.lib1
+{
+    public class Calcolatrice
+    {
+        public Calcolatrice()
+        {
+            PublicKey double somma(double a, double b)
+            {
+                return x + y;
+            }    
+                
+        }
+    }
+}
---
+++ ConsoleApp1/TestCalcolatrice/Test1.cs
@@ -0,0 +1,19 @@
+﻿namespace TestCalcolatrice
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {           
+            Calcolatricelib.calcolatrice calcolatrice = new Calcolatricelib.calcolatrice();
+            double risultato = calcolatrice.somma(2, 3);
+            Assert.AreEqual(5, risultato,"La somma di 2 e 3 dovrebbe essere 5");
+        }   
+        [TestMethod]
+        public void Testsommaconzero() {
+            Calcoltrice calcoltrice = new Calcoltrice();
+            double risultato = calcoltrice.somma(0, 3);
+            Assert.AreEqual(3, risultato, "La somma di 0 e 3 dovrebbe essere 3");    
+        }
+}  
